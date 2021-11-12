105f178b3ed60d365e025a7c2995291446199d7a
AlfieriRiccardo <42537112+AlfieriRiccardo@users.noreply.github.com>
2021-11-12 14:38:34 +0100

    Start prj

--- src/prj.js
+++ src/prj.js
@@ -1,36 +1,110 @@
+////////////////////////////////////////////////////////////////////////
+                              /*INIT*/
+////////////////////////////////////////////////////////////////////////
 
-/*
-SPAWN PUNTINI
-*/
+const canvas = document.getElementById('canvas');
+const ctxq = canvas.getContext("2d");
+var w = canvas.width;
+var h = canvas.height;
 
-var w = document.getElementById('canvas').width;
-var h = document.getElementById('canvas').height;
-console.log(w, h);
-function getPos(){
-  return [w * Math.random(), h * Math.random()];
+
+////////////////////////////////////////////////////////////////////////
+                              /*DOTS*/
+////////////////////////////////////////////////////////////////////////
+
+function getPosDot() {
+  var rand = Math.random();
+  if (rand >= 0 && rand<= 0.5){
+    var x = w * Math.random();
+    var y = h/2 * Math.random();
 }
-
-function positions(){
-  var [x, y] = getPos(); 
-  return [x, y];
-}
-
-const c = document.getElementById('canvas');
-
-function point(x, y, canvas){
-  canvas.beginPath();
-  canvas.arc(x, y, 1, 0, 2 * Math.PI, true);
-  canvas.stroke();
-}
-
-ctx = c.getContext("2d");
-const pos = [];
-
-for (j = 0; j < 2000; j++){
-  pos[j] = positions();
-  console.log(pos[j][0], pos[j][1]);
-  point(pos[j][0], pos[j][1], ctx);
+  else{
+    var x = w * Math.random();
+    var y = h/2 + ( h/2 * Math.random());
+  }
+  var posDot = [];
+  posDot = [x, y];
+  return posDot;
 }
 
 
+
+////////////////////////////////////////////////////////////////////////
+                              /*NEURONI*/
+////////////////////////////////////////////////////////////////////////
+
+class Neurons{
+  constructor(n, width, height, ctx){
+    this.n = n;
+    this.width = width;
+    this.height = height;
+    this.ctx = ctx;
+    this.posNeu = [];
+  }
+
+  setPosNeu(){
+    const pos = [];
+    for(var i = 0; i < this.n; i++){
+        var [x, y] = [this.width*Math.random(), this.height*Math.random()];
+        pos[i] = [x, y];
+    }
+    this.posNeu = pos;
+    return pos;
+  }
+  
+  initNeurons(pos){
+    for(var i = 0; i < pos.length; i++){
+      this.ctx.arc(pos[i][0], pos[i][1], 5, 0, 2 * Math.PI, true);
+      this.ctx.strokeStyle = "red";
+      this.ctx.stroke();
+      if ( i == 0){
+          this.ctx.moveTo(pos[i][0], pos[i][1]);
+          this.ctx.stroke();
+      }
+      else{
+          this.ctx.lineTo(pos[i][0], pos[i][1]);
+          this.ctx.lineWidth = 0.5;
+          this.ctx.stroke();
+      }
+    }
+  }
+
+  init(){
+    var posNeu = this.setPosNeu();
+    this.initNeurons(posNeu);
+  }
+  
+  learning(posDot){
+    var min = 0;
+    var distmin = 0;
+    for(var i = 0; i < this.posNeu.length; i++){
+      var x1 = this.posNeu[i][0];
+      var y1 = this.posNeu[i][1];
+      var x2 = posDot[0];
+      var y2 = posDot[1];
+      var dist = Math.hypot(x2 - x1, y2 - y1);
+      if (i == 0){
+          distmin = dist;
+          min = i;
+      }
+      else if (dist < distmin){
+          min = i;
+          distmin = dist;
+      }
+    }
+  }
+
+  
+}
+
+const n1 = new Neurons(100, w, h, ctxq);
+n1.init();
+
+function start(){
+  var posDot = [];
+  posDot = getPosDot();
+  n1.learning(posDot);
+}
+
+//setInterval(() => start(), 1000) //set interval con getposdot
 
