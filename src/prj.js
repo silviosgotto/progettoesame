fcabcf797cb0a971d9b06e8006b2a33d30cb2b65
AlfieriRiccardo <42537112+AlfieriRiccardo@users.noreply.github.com>
2021-11-17 15:00:01 +0100

    :))))

--- src/prj.js
+++ src/prj.js
@@ -1,89 +1,108 @@
 ////////////////////////////////////////////////////////////////////////
                               /*INIT*/
 ////////////////////////////////////////////////////////////////////////
+const c = new AudioContext();
+var bpm = 240;
+var oscillators = [];
 
-const canvas = document.getElementById('canvas');
-const ctxq = canvas.getContext("2d");
-var w = canvas.width;
-var h = canvas.height;
+const kick = document.getElementById('kick');
+const ctxKick = kick.getContext("2d");
+const wKick = kick.width;
+const hKick = kick.height;
 
+const snare = document.getElementById('snare');
+const ctxSnare = snare.getContext("2d");
+const wSnare = snare.width;
+const hSnare = snare.height;
 
-////////////////////////////////////////////////////////////////////////
-                              /*DOTS*/
-////////////////////////////////////////////////////////////////////////
-
-function getPosDot() {
-  var x = w * Math.random();
-  var y = h * Math.random();
-  var posDot = [];
-  posDot = [x, y];
-  return posDot;
-}
-
-
+const PlayButt = document.getElementById('play');
+PlayButt.disabled = "disabled";
 
 ////////////////////////////////////////////////////////////////////////
                               /*NEURONI*/
 ////////////////////////////////////////////////////////////////////////
 
 class Neurons{
-  constructor(n, width, height, ctx){
+  constructor(n, width, height, ctx, shape){
     this.n = n;
     this.width = width;
     this.height = height;
     this.ctx = ctx;
     this.posNeu = [];
+    this.shape = shape;
+    this.sigma = 20;
+    this.eps = 0.2;
+    this.count = 0;
   }
 
-  setPosNeu(){
-    const pos = [];
-    for(var i = 0; i < this.n; i++){
-        var [x, y] = [(this.width)*Math.random(), (this.height)*Math.random()];
-        pos[i] = [x, y];
-    }
-    this.posNeu = pos;
-    return pos;
+  getShape(){
+    return this.shape;
   }
 
   getposNeu(){
     return this.posNeu;
   }
-  
-  initNeurons(pos){
-    this.ctx.beginPath();
-    for(var i = 0; i < pos.length; i++){
-      this.ctx.arc(pos[i][0], pos[i][1], 1, 0, 2 * Math.PI, true);
-      this.ctx.strokeStyle = "red";
-      this.ctx.stroke();
-      /*if ( i == 0){
-          this.ctx.moveTo(pos[i][0], pos[i][1]);
-          //this.ctx.stroke();
+
+  getEps(){
+    return this.eps;
+  }
+
+  getId(){
+    return thi.id;
+  }
+
+  setPosNeu(){
+    if(this.shape == "rect"){
+      for(var i = 0; i < this.n; i++){
+        var [x, y] = [(this.width)*Math.random(), (this.height)*Math.random()];
+        this.posNeu[i] = [x, y];
       }
-      else{
-          this.ctx.lineTo(pos[i][0], pos[i][1]);
-          this.ctx.lineWidth = 0.5;
-          //this.ctx.stroke();
-      }*/
-      
+    }
+    else if(this.shape == "tri"){
+      for(var i = 0; i < this.n; i++){
+        var a = Math.random();
+        var b = Math.random();
+        var asqrt = Math.sqrt(a);
+        var [x, y] = [(1 - asqrt) * 0 + asqrt * (1 - b) * (this.width / 2) + asqrt * b * this.width,  (1 - asqrt) * this.height + asqrt * (1 - b) * 0 + asqrt * b * this.height]
+        this.posNeu[i] = [x, y];
+      }
     }
   }
 
   init(){
-    var posNeu = this.setPosNeu();
-    this.initNeurons(posNeu);
+    this.setPosNeu();
+    this.drawNeurons();
   }
 
-  drawNeurons(posNeu){
+  drawNeurons(){
     this.ctx.beginPath();
-    for(var i = 0; i < posNeu.length; i++){
-      this.ctx.arc(posNeu[i][0], posNeu[i][1], 1, 0, 2 * Math.PI, true);
+    for(var i = 0; i < this.posNeu.length; i++){
+      this.ctx.arc(this.posNeu[i][0], this.posNeu[i][1], 1, 0, 2 * Math.PI, true);
     }
     this.ctx.stroke();
   }
+
+  getPosDot() {
+    if(this.shape == "rect"){
+      var x = this.width * Math.random();
+      var y = this.height * Math.random();
+      return [x, y];
+    }
+    else if(this.shape == "tri"){
+      var r1 = Math.random();
+      var r2 = Math.random();
+      var sqrtr1 = Math.sqrt(r1);
+    
+      var x = (1 - sqrtr1) * 0 + (sqrtr1 * (1 - r2) * this.width) / 2 + sqrtr1 * r2 * this.width;
+      var y = (1 - sqrtr1) * this.height + sqrtr1 * (1 - r2) * 0 + sqrtr1 * r2 * this.height;
+      return [x, y];
+    }
+  }
   
-  learning(posDot, sigma, eps){
+  learning(){
     var min = 0;
     var distmin = 0;
+    let posDot = this.getPosDot();
     var x2 = posDot[0];
     var y2 = posDot[1];
     for(var i = 0; i < this.posNeu.length; i++){
@@ -101,41 +120,128 @@ class Neurons{
     }
 
     for(var j = 0; j < this.posNeu.length; j++){
-      this.posNeu[j][0] = this.posNeu[j][0] + eps * Math.exp(-Math.pow(j-min, 2) / (2 * Math.pow(sigma, 2))) * (x2 - this.posNeu[j][0]);
-      this.posNeu[j][1] = this.posNeu[j][1] + eps * Math.exp(-Math.pow(j-min, 2) / (2 * Math.pow(sigma, 2))) * (y2 - this.posNeu[j][1]);
+      this.posNeu[j][0] = this.posNeu[j][0] + this.eps * Math.exp(-Math.pow(j-min, 2) / (2 * Math.pow(this.sigma, 2))) * (x2 - this.posNeu[j][0]);
+      this.posNeu[j][1] = this.posNeu[j][1] + this.eps * Math.exp(-Math.pow(j-min, 2) / (2 * Math.pow(this.sigma, 2))) * (y2 - this.posNeu[j][1]);
     }
+    this.drawNeurons();
+    if (this.count % 5 == 0){
+      this.sigma = 0.95 * this.sigma;
+      this.eps = 0.999 * this.eps;
+    }
+    this.count++;
+  }
 
-    this.drawNeurons(this.posNeu);
-    
+  normalizeArray(arr){
+    let min = Math.min(...arr);
+    let max = Math.max(...arr);
+
+    for(let i = 0; i<arr.length; i++){
+      arr[i] = (arr[i]-min)/(max-min);
+      if (arr[i] >= 0 && arr[i] < 0.25){
+        arr[i] = 1;
+      }
+      else if( arr[i] >= 0.25 && arr[i]<0.50){
+        arr[i] = 2;
+      }
+      else if(arr[i] >= 0.50 && arr[i] < 0.75){
+        arr[i] = 3;
+      }
+      else{
+        arr[i] = 4;
+      }
+    }
+    return arr;
+  }
+
+  calcDistNormNeu(){
+    var dist = [];
+    for(var i = 1; i<this.posNeu.length; i++){
+      let x2 = this.posNeu[i][0];
+      let y2 = this.posNeu[i][1];
+      let x1 = this.posNeu[i-1][0];
+      let y1 = this.posNeu[i-1][1];
+      dist[i-1] = Math.hypot(x2 - x1, y2 - y1);
+    }
+    this.normalizeArray(dist);
+    return dist;
   }
 }
 
-var stopped = false;
 
-var sigma = 18;
-var eps = 0.2;
-var count = 0;
-const n1 = new Neurons(50, w, h, ctxq, sigma, eps);
+////////////////////////////////////////////////////////////////////////
+                            /*OSCILLATOR*/
+////////////////////////////////////////////////////////////////////////
+
+class Oscillator{
+  constructor(ctx, frequency, arrDist){
+    this.ctx = ctx;
+    this.frequency = frequency;
+    this.arrDist = arrDist;
+    this.i = 0;
+  }
+
+  initOsc(){
+    let o = this.ctx.createOscillator();
+    o.frequency.value = this.frequency;
+    o.connect(this.ctx.destination);
+    let startTime = c.currentTime;
+    console.log(startTime);
+    o.start(startTime);
+    o.stop(startTime + 0.1);
+  }
+
+  play(){
+    this.initOsc();
+    this.i++;
+    if(this.i < this.arrDist.length){
+      setTimeout(() => this.play(), (60/bpm)*1000*this.arrDist[this.i-1]);
+    }
+    else{
+      this.i = 0;
+      return;
+    }
+  }
+}
+
+var n = 32;
+const n1 = new Neurons(n, wKick, hKick, ctxKick, "rect");
+const n2 = new Neurons(n, wSnare, hSnare, ctxSnare, "tri");
 n1.init();
+n2.init();
 
-function start(){
-  if (eps <= 0.1){
+function startLearning(Neurons, canvas, ctx, id, freq){
+  if (Neurons.getEps() <= 0.1){
     clearInterval(id);
-    stopped = true;
-    console.log("fermo");
+    console.log(Neurons.calcDistNormNeu());
+    const osc = new Oscillator(c, freq, Neurons.calcDistNormNeu());
+    oscillators.push(osc);
+    PlayButt.disabled = "";
+
   }
-  ctxq.clearRect(0, 0, w, h);
-  var posDot = [];
-  posDot = getPosDot();
-  n1.learning(posDot, sigma, eps);
-  if (count % 5 == 0){
-    sigma = 0.96 * sigma;
-    eps = 0.999 * eps;
-    console.log(count, eps);
-  }
-  count++;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  Neurons.learning();
 }
-var id = setInterval(() => start(), 5);
+
+function createOsc(){
+  var idKick = setInterval(() => startLearning(n1, kick, ctxKick, idKick, 261.63), 2);
+  var idSnare = setInterval(() => startLearning(n2, snare, ctxSnare, idSnare, 440), 2);
+} 
+createOsc();
+
+
+function startPlaying(){
+  console.log(oscillators); //fare la funzione suono
+  var time = c.currentTime;
+  for (var i = 0; i<oscillators.length; i++){
+    oscillators[i].play();
+  }
+}
+
+
+PlayButt.onclick = () => startPlaying();
+
+
+
 
 
 
