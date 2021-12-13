aba28f236b27f1eff1bf4f7078f67ac1fbdc01d9
AlfieriRiccardo <42537112+AlfieriRiccardo@users.noreply.github.com>
2021-12-13 13:01:33 +0100

    cose

--- src/MelodicSound.js
+++ src/MelodicSound.js
@@ -14,23 +14,39 @@ module.exports = class MelodicSound {
     }
 
     rightLoop(){
-        var loopEnd = Math.ceil(this.arrDur.at(-1));
+        /* var loopEnd = Math.ceil(this.arrDur.at(-1));
         if (loopEnd % this.metrica != 0) {
             loopEnd = loopEnd + (this.metrica - (loopEnd % this.metrica));
         }
-        return loopEnd;
+        return loopEnd; */
+        var loopEnd = this.arrDur.at(-1)
+        if (loopEnd % this.metrica == 0){
+            loopEnd = loopEnd + this.metrica
+        }
+        else{
+            loopEnd = Math.ceil(this.arrDur.at(-1))
+            if (loopEnd % this.metrica != 0) {
+                loopEnd = loopEnd + (this.metrica - (loopEnd % this.metrica))
+            }
+        }
+        return loopEnd
     }
 
     createPart(){
+        /* var bpm = 60/this.bpm; */
         var bpm = 60/this.bpm;
+        this.part.loopStart=0;
+        this.part.loopEnd = this.rightLoop()*bpm;
         for (var j = 0; j<this.arrDur.length; j++){
             this.arrDur[j] = this.arrDur[j]*bpm;
             this.part.add({time: this.arrDur[j], note: this.baseNote*Math.pow(2, (this.arrPos[j] - 1)/12)});
         }
-        this.part.loopStart=0;
+        /* this.part.loopStart=0;
         this.part.loopEnd = this.rightLoop();
         this.part.loop = true;
         console.log(this.arrDur);
-        console.log(this.rightLoop());
+        console.log(this.rightLoop()); */
+        this.part.loop = true;
+        console.log(this.arrDur);
     }
 }
